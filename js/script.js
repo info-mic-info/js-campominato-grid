327ff1f32b4dfde2146c9d9d26a66ebba9ec1331
info-mic-info <jobsocial94@gmail.com>
2022-12-06 17:59:43 +0100

    structure of grid

---
+++ js/script.js
@@ -0,0 +1,43 @@
+
+
+// Questa funzione crea una "div", gli aggiunge una "classe=square" e con return la restituisce
+// ******************************************************************************
+function createGridSquare(number)
+{
+
+// Crea un div
+const currentElement= document.createElement (`div`);
+
+// Aggiunge al Div la classe=square
+currentElement.classList.add(`square`);
+
+currentElement.innerHTML= number;
+
+// Restituzione
+return currentElement;
+}
+// ******************************************************************************
+
+
+
+
+// Dichiaro la variabile "let grid" e la vado ad inserire nel div con "id=grid"
+let grid = document.getElementById (`grid`);
+
+
+// Ciclo che permette di generare 100 celle
+// ******************************************************************************
+for (let i=0; i<100; i++){
+const currentSquare = createGridSquare(i+1);
+
+
+// Con l'evento "click", mediante la classe "clicked", poi stilizzata in CSS, impartiamo il comando di far diventare ogni singola cella di colore azzurro ogni volta che ci clicchiamo sopra.
+currentSquare.addEventListener (`click`, function(){
+this.classList.add (`clicked`);
+});
+
+// "appendChild" crea un figlio all'interno dell'elemento che seleziono, in questo caso "grid". Risultato, all'interno dell'HTML dell'elemento con id "grid" che è collegato alla variabile "grid" di javascript, vengono creati "div" pari al numero del ciclo For.
+grid.appendChild(currentSquare);
+
+}
+// ******************************************************************************
